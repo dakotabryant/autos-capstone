b6f84bacfcd701d698b6d74b036ed0fce151c9ef
Dakota Bryant <bryant.dakota@gmail.com>
2017-05-10 17:37:43 -0500

    DONE. All endpoints, and front end stuff at least working

--- scripts/app.js
+++ scripts/app.js
@@ -33,14 +33,15 @@ $(document).ready(function() {
 	renderCars()
 	$('.inventory-container').on('click', 'div.car-container', function() {
 		$(this).children('.hidden').toggleClass('hidden');
-    $(this).children('p').toggleClass('hidden');
+		$(this).children('p').toggleClass('hidden');
 	})
+
+	//PUT event listeners
 	$('.inventory-container').on('click', 'button.submit', function(e) {
-    $(this).toggleClass('hidden');
-		$(this).siblings('.edits').toggleClass('hidden')
+		$(this).toggleClass('hidden');
+		$(this).siblings('.edits').toggleClass('hidden');
 		let elementId = $(this).parents('.car-container').attr('data-id');
-    console.log(e.target);
-    let inputFinder = $(e.target).closest('div');
+		let inputFinder = $(e.target).closest('div');
 		let newObject = {
 			make: inputFinder.find('.input-make').val(),
 			model: inputFinder.find('.input-model').val(),
@@ -49,22 +50,65 @@ $(document).ready(function() {
 		};
 		fetch(`http://localhost:8080/cars/${elementId}`, {
 				method: 'PUT',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
 				body: JSON.stringify(newObject)
 			})
 			.then(res => {
 				if (res.status === 201) {
 					getData(endpointURL)
 					renderCars();
-          console.log('I sent the request');
+					console.log('I sent the request');
 				} else {
 					alert('something went wrong, try again')
 				}
 			})
 
 	});
+	//delete handler
+	$('.inventory-container').on('click', 'button.delete', function() {
+		$(this).siblings('.edits').toggleClass('hidden')
+		let elementId = $(this).parents('.car-container').attr('data-id');
+		fetch(`http://localhost:8080/cars/${elementId}`, {
+				method: 'DELETE'
+			})
+			.then(res => {
+				if (res.status === 204) {
+					getData(endpointURL)
+					renderCars();
+				}
+			})
+	})
+	//modal handler
+	$('#create').on('click', function() {
+		$('.overlay, .new-listing').toggleClass('hidden');
+		$('body').css('overflow', 'hidden')
+	})
+	//post handler
+	$('#submit-create').on('click', function(e) {
+		$('.overlay, .new-listing').toggleClass('hidden');
+		$('body').css('overflow', 'auto');
+		let inputFinder = $(e.target).closest('div');
+		let newObject = {
+			make: inputFinder.find('.submit-make').val(),
+			model: inputFinder.find('.submit-model').val(),
+			price: inputFinder.find('.submit-price').val(),
+			year: inputFinder.find('.submit-year').val()
+		};
+		fetch(`http://localhost:8080/cars`, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(newObject)
+			})
+			.then(res => {
+				getData(endpointURL)
+				renderCars();
+			})
+	})
 
 })
